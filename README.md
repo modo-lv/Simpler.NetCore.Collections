ef49d21d126d1dcc7953213e58d308865ea945cd
MoDo <modo@modo.lv>
2020-05-18 19:34:22 +0300

    Added `Nil`.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-﻿# Simpler.NetCore.Collections
+# Simpler.NetCore.Collections
 
 ## Extension methods
 
@@ -11,7 +11,7 @@ A fluent alternative to `Enumerable.Contains`.
 ```cs
 "x".IsOneOf(new List<String> { "a", "b", "c" }); // false
 "b".IsOneOf("a", "b", "c");                      // true
-``` 
+```
 
 ### `StringJoin`
 
@@ -22,5 +22,19 @@ A fluent alternative to `String.Join`.
 ```cs
 new []{"a","b","c"}.StringJoin();     // "abc"
 new []{"a","b","c"}.StringJoin(", "); // "a, b, c"
-``` 
+```
 
+## Nil
+
+Contains shorthand methods and properties for instantiating common collections.
+
+##### Methods
+
+* `E()` creates an empty Enumerable.
+* `L()` Creates an empty List.
+* `S()` Creates an empty HashSet.
+* `D()` Creates an empty Dictionary.
+
+##### Properties
+
+**`*Str`** properties for each type instantiate the corresponding collection of `String`s. For `Dictionary` it's `DStr<TValue>`, which creates a dictionary of `String` keys and `TValue` values.
---
+++ Simpler.NetCore.Collections/Nil.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Simpler.NetCore.Collections {
+  /// <summary>
+  /// Shorthand methods for instantiating empty collections.
+  /// </summary>
+  /// <remarks>
+  /// <c>E</c>: An empty <see cref="Enumerable"/>.<br />
+  /// <c>L</c>: An empty <see cref="List{T}"/>.<br />
+  /// <c>S</c>: An empty <see cref="HashSet{T}"/>.<br />
+  /// <c>D</c>: An empty <see cref="Dictionary{T, T}"/>.<br />
+  /// <c>*Str</c>: An empty collection of type <see cref="String"/>
+  /// (both key and value in case of <see cref="Dictionary{TKey,TValue}" />).<br />
+  /// </remarks>
+  public static class Nil {
+    /// <summary>
+    /// Create an empty enumerable.
+    /// </summary>
+    public static IEnumerable<T> E<T>() => Enumerable.Empty<T>();
+
+    /// <inheritdoc cref="E{T}"/>
+    public static IEnumerable<String> EStr => E<String>();
+
+
+    /// <summary>
+    /// Create an empty <see cref="List{T}"/>.
+    /// </summary>
+    public static IList<T> L<T>() => new List<T>();
+
+    /// <inheritdoc cref="L{T}"/>
+    public static IList<String> LStr => L<String>();
+
+
+    /// <summary>
+    /// Create an empty <see cref="HashSet{T}"/>.
+    /// </summary>
+    public static ISet<T> S<T>() => new HashSet<T>();
+
+    /// <inheritdoc cref="S{T}"/>
+    public static ISet<String> SStr => S<String>();
+
+
+    /// <summary>
+    /// Create an empty <see cref="Dictionary{TKey,TValue}"/>.
+    /// </summary>
+    public static IDictionary<TKey, TVal> D<TKey, TVal>() => new Dictionary<TKey, TVal>();
+
+    /// <inheritdoc cref="D{TKey,TVal}"/>
+    public static IDictionary<T, T> D<T>() => D<T, T>();
+
+    /// <inheritdoc cref="D{TKey,TVal}"/>
+    public static IDictionary<String, TValue> DStr<TValue>() => D<String, TValue>();
+  }
+}
